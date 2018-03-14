ff16421f3fd28700431e6be401fc54462db8799a
wangliansheng <8863704@qq.com>
2018-03-14 12:23:41 +0800

    bbb

--- niot-python.py
+++ niot-python.py
@@ -1,12 +1,17 @@
 from flask import Flask
 import config
 from exts import db
+from models import user
 app = Flask(__name__)
 app.config.from_object(config)
+
 db.init_app(app)
 
 @app.route('/')
 def hello_world():
+	newuser=user(username="wangwen",password="111111")
+	db.session.add(newuser)
+	db.session.commit()
 	return 'Hello World!'
 
 
